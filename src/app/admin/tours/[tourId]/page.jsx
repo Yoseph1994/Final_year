cc9506d1c9629430e8e0d04659a5b93545d9303c
Yoseph Shimelis <joshrde2002@gmail.com>
2024-06-13 00:14:58 +0300

    Update page.jsx

--- src/app/admin/tours/[tourId]/page.jsx
+++ src/app/admin/tours/[tourId]/page.jsx
@@ -1,5 +1,6 @@
 import AdminTourUpdate from "@/components/uiComponents/AdminTourUpdate";
 import ErrorAlert from "@/components/uiComponents/ErrorAlert";
+import { fetchGuides, fetchLeadGuides } from "@/lib/actions/users";
 import { fetchTourById } from "@/lib/actions/tours";
 import React from "react";
 
@@ -7,11 +8,15 @@ async function page({ params }) {
   const tourId = params.tourId;
   const tourRes = await fetchTourById(tourId);
   const tour = tourRes ? JSON.parse(tourRes) : null;
+  const guidesRes = await fetchGuides();
+  const guides = guidesRes ? JSON.parse(guidesRes) : [];
+  const leadGuidesRes = await fetchLeadGuides();
+  const leadGuides = leadGuidesRes ? JSON.parse(leadGuidesRes) : [];
 
   return (
     <div>
       {tour ? (
-        <AdminTourUpdate tour={tour} />
+        <AdminTourUpdate tour={tour} guides={guides} leadGuides={leadGuides} />
       ) : (
         <ErrorAlert
           description={"Please check you connection and try again."}
