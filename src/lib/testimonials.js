b23af57eac648a78d950f6097bd4939900d144d3
Yoseph Shimelis <joshrde2002@gmail.com>
2024-06-12 23:50:09 +0300

    Update testimonials.js

--- src/lib/testimonials.js
+++ src/lib/testimonials.js
@@ -1,72 +1,74 @@
 const testimonials = [
   {
+    // https://images.unsplash.com/photo-1633332755192-727a05c4013d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D
     image:
-      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D",
-    name: "Alice Johnson",
+      "https://res.cloudinary.com/dgtrmhlsp/image/upload/v1718221070/370ccb4d-068e-41bf-af6a-4d7527862ca5_oxzyok.jpg",
+    name: "Anteneh Shimelis",
     review:
       "An amazing experience! The tour was well organized and the sights were breathtaking.",
     rating: 4,
   },
+  // https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dXNlcnxlbnwwfHwwfHx8MA%3D%3D
   {
     image:
-      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dXNlcnxlbnwwfHwwfHx8MA%3D%3D",
-    name: "Michael Smith",
+      "https://res.cloudinary.com/dgtrmhlsp/image/upload/v1718220683/9ff0b546-1f8d-415e-89f4-749d7ccbd5e0_mfvtwf.jpg",
+    name: "Antishetif Tamiru",
     review:
       "Loved every moment of the trip! Highly recommended for anyone looking for a great adventure.An amazing experience! The tour was well organized and the sights were breathtaking.",
     rating: 5,
   },
   {
     image:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D",
-    name: "Emily Davis",
+      "https://res.cloudinary.com/dgtrmhlsp/image/upload/v1718221073/8ce7b65d-41e2-42da-8661-a7a3f163b375_up29fz.jpg",
+    name: "Rajif Dafis",
     review:
       "A wonderful tour with fantastic guides. The scenery was incredible and the service top-notch.",
     rating: 4,
   },
   {
     image:
-      "https://images.unsplash.com/photo-1560250097-0b93528c311a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8fDA%3D",
-    name: "John Brown",
+      "https://res.cloudinary.com/dgtrmhlsp/image/upload/v1718221902/44618f26-93a7-48be-abd4-e8623fd92caf_z0z1ou.jpg",
+    name: "Kidist Deriba",
     review:
       "Good tour overall, but a few minor issues with the schedule. Still, a great way to see the area.An amazing experience! The tour was well organized and the sights were breathtaking. An amazing experience! The tour was well organized and the sights were breathtaking",
     rating: 3,
   },
   {
     image:
-      "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D",
-    name: "Sophia Wilson",
+      "https://res.cloudinary.com/dgtrmhlsp/image/upload/v1718221564/51fdb4e5-2fc0-4e1c-a6ca-9916717caeae_fk7zid.jpg",
+    name: "Bethel Gizachew",
     review:
       "Fantastic experience! The tour guide was knowledgeable and very friendly. Highly recommended.An amazing experience! The tour was well organized and the sights were breathtaking",
     rating: 5,
   },
   {
     image:
-      "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHVzZXJ8ZW58MHx8MHx8fDA%3D",
-    name: "Oliver Martinez",
+      "https://res.cloudinary.com/dgtrmhlsp/image/upload/v1718221904/2059a6ec-f6c2-4963-824f-2fa6ff49c031_tarmh4.jpg",
+    name: "Kenenisa K.",
     review:
       "An enjoyable tour with great sights. The itinerary was well planned and the guide was excellent.",
     rating: 4,
   },
   {
     image:
-      "https://images.unsplash.com/photo-1619895862022-09114b41f16f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHVzZXJ8ZW58MHx8MHx8fDA%3D",
-    name: "Mia Thomas",
+      "https://res.cloudinary.com/dgtrmhlsp/image/upload/v1718221570/f59c88b4-0ba8-4c3c-9456-b162fd55086a_iccfn1.jpg",
+    name: "Misikir Digafe",
     review:
       "I had a great time on this tour. The locations were beautiful and the service was fantastic.",
     rating: 5,
   },
   {
     image:
-      "https://images.unsplash.com/photo-1556157382-97eda2d62296?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fHw%3D",
-    name: "Noah Jackson",
+      "https://res.cloudinary.com/dgtrmhlsp/image/upload/v1718221743/6bad24b5-5852-4efa-92c1-2ba1d0a464cc_amj2ug.jpg",
+    name: "Ermias Demsu",
     review:
       "A decent tour, though there were a few things that could be improved. Overall, a good experience.An amazing experience! The tour was well organized and the sights were breathtaking",
     rating: 3,
   },
   {
     image:
-      "https://images.unsplash.com/photo-1589386417686-0d34b5903d23?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE1fHx8ZW58MHx8fHx8",
-    name: "Evelyn White",
+      "https://res.cloudinary.com/dgtrmhlsp/image/upload/v1718221572/879431dc-5476-4827-88ce-ac98c091f798_ckmplq.jpg",
+    name: "Seena Tolera",
     review:
       "Loved it! The tour was well-organized and the places we visited were stunning.",
     rating: 5,
