f3c293f095c793976c9004ce2b239e18d5118940
Yoseph Shimelis <joshrde2002@gmail.com>
2024-07-03 22:33:00 +0300

    Update tours.js

--- src/lib/actions/tours.js
+++ src/lib/actions/tours.js
@@ -255,7 +255,7 @@ export const fetchClosestTour = async (lat, long) => {
     await connectDB();
     const closeTours = await tourModel
       .find({
-        startLocation: {
+        location: {
           $near: {
             $geometry: {
               type: "Point",
